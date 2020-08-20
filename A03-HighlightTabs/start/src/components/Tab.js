54e85648ea4f5ca6efb72d61cc6c769a3308832c
Max Ponomarenko <maxeleron@gmail.com>
2020-08-20 11:06:36 +0300

    A03-HighlightTabs
    -Learned basics of components
    -Learned how to use external components;
    -Learned how to use React Router

---
+++ A03-HighlightTabs/start/src/components/Tab.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function Tab({ children }) {
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  function moveHighlight(e) {
+    // update highlightStyle to move the highlight
+    setHighlightStyle({
+      left: e.nativeEvent.layerX - 150,
+    });
+  }
+
+  function hideHighlight(e) {
+    setHighlightStyle({
+      opacity: 0,
+      left: e.nativeEvent.layerX - 150,
+    });
+  }
+
+  return (
+    <div className="tab" onMouseMove={moveHighlight} onMouseOut={hideHighlight}>
+      <div className="highlight" style={highlightStyle} />
+      {children}
+    </div>
+  );
+}
+
+export default Tab;
