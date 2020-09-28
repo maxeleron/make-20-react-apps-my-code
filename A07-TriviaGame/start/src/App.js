6607fb0864850b709874d2d4ebdbb782fdea6268
Max Ponomarenko <maxeleron@gmail.com>
2020-09-28 14:35:33 +0300

    A07-TriviaGame-Selecting_a_category

--- A07-TriviaGame/start/src/App.js
+++ A07-TriviaGame/start/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Question from './components/Question';
 import CategorySelector from './components/CategorySelector';
 import ResultModal from './components/ResultModal';
@@ -7,18 +7,23 @@ import './App.css';
 
 export default function App() {
   const [question, setQuetstion] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState('any');
 
-  useEffect(() => {
-    getQuestion();
-  }, []);
+  const getQuestion = useCallback(() => {
+    let url = 'https://opentdb.com/api.php?amount=10';
 
-  function getQuestion() {
-    const url = 'https://opentdb.com/api.php?amount=10';
+    if (selectedCategory !== 'any') url += `&category=${selectedCategory}`;
+
+    console.log(url);
 
     fetch(url)
       .then((res) => res.json())
       .then((data) => setQuetstion(data.results[0]));
-  }
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    getQuestion();
+  }, [getQuestion, selectedCategory]);
 
   return (
     <div className="app">
@@ -27,7 +32,10 @@ export default function App() {
 
       {/* question header ----------------------- */}
       <div className="question-header">
-        <CategorySelector />
+        <CategorySelector
+          category={selectedCategory}
+          chooseCategory={setSelectedCategory}
+        />
         <Scoreboard />
       </div>
 
